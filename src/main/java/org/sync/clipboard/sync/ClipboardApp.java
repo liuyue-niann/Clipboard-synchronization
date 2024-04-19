0ed1b14a62c03fe2e7999bd93a82ed5bbbe46bbf
niann <liuyue.niann@gmail.com>
2024-04-20 05:59:25 +0800

    repair:修复剪贴板内容重复问题

--- src/main/java/org/sync/clipboard/sync/ClipboardApp.java
+++ src/main/java/org/sync/clipboard/sync/ClipboardApp.java
@@ -2,6 +2,7 @@ package org.sync.clipboard.sync;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sync.clipboard.listen.ClipboardListen;
 import org.sync.clipboard.utils.ImgUtils;
 
 import java.awt.*;
@@ -39,6 +40,7 @@ public class ClipboardApp {
                 clipboard.setContents(selection, null);
                 log.info("写入剪贴板图片:{}", text);
             }
+            ClipboardListen.setLastClipboardContent(getClipboardContent());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
